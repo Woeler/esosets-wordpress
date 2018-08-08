395f97adee8bce59fd3d02852f6fad161da8b8b0
Woeler <woeler@esoleaderboards.com>
2018-08-08 20:27:28 +0200

    Add Skills

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # EsoSets Wordpress
 
-Welcome to the eso-sets plugin for Wordpress. With this plugin you can show tooltips in Wordpress with gear set information about the Elder Scrolls Online. All data is directly pulled from https://eso-sets.com
+Welcome to the eso-sets plugin for Wordpress. With this plugin you can show tooltips in Wordpress with gear set information about the Elder Scrolls Online. All data is directly pulled from https://eso-sets.com and https://eso-skillbook.com
 
+## Sets
 In order to use this plugin you must use shortcodes in your Wordpress posts and pages. You need the set id of a specific set. Lets say you want to include the Death's Wind set, which has id 1. You would use the following shortcode.
 
 ```[esoset id="1"]```
@@ -9,5 +10,14 @@ In order to use this plugin you must use shortcodes in your Wordpress posts and 
 Note that some Wordpress themes may need some help recognizing this is a tooltip. If the above code does not work, try the following.
 
 ```[esoset id="1" tooltip="true"]```
+
+## Skills
+In order to use this plugin you must use shortcodes in your Wordpress posts and pages. You need the set id of a specific skill. Lets say you want to include the Dragonknight Standard skill, which has id 8. You would use the following shortcode.
+
+```[esoskill id="8"]```
+
+Note that some Wordpress themes may need some help recognizing this is a tooltip. If the above code does not work, try the following.
+
+```[esoskill id="8" tooltip="true"]```
 
 Please do not access the eso-sets api with a modified version of this plugin. Accessing the API with a modified version of this plugin may result in the permanent suspension of your domain from the eso-sets api.
--- eso-sets.php
+++ eso-sets.php
@@ -44,7 +44,7 @@ final class EsoSets
         if (!isset($atts['id'])) {
             return 'Set id not provided';
         }
-		
+
         $cache = get_transient(md5('esosets_' . serialize($atts)));
 
         if ($cache) {
@@ -54,16 +54,16 @@ final class EsoSets
         $ch = curl_init('https://eso-sets.com/api/tooltip/set/' . $atts['id']);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$ex = curl_exec($ch);
+        $ex = curl_exec($ch);
         $result = json_decode($ex, true);
         curl_close($ch);
 
         if (!isset($result['name']) || !isset($result['tooltip'])) {
-			if (isset($atts['setname'])) {
-            	return $atts['setname'];
-        	} else {
-            	return 'An error occurred retrieving the set data';
-			}
+            if (isset($atts['setname'])) {
+                return $atts['setname'];
+            } else {
+                return 'An error occurred retrieving the set data';
+            }
         }
 
         $tooltip = str_replace('"', "'", $result['tooltip']);
@@ -77,10 +77,63 @@ final class EsoSets
             $return .= 'data-toggle="tooltip" ';
         }
         $return .= 'data-html="true" title="' . $tooltip . '">' . $name . '</a>';
-		
+
         if (!is_preview()) {
 
             set_transient(md5('esosets_' . serialize($atts)), $return, 3600);
+
+        }
+        return $return;
+    }
+
+    /**
+     * Convert all esoskillbook shortcode elements to tooltips upon post/page save
+     *
+     * @param $atts
+     * @return string
+     */
+    public function esoskill_func($atts)
+    {
+        if (!isset($atts['id'])) {
+            return 'Skill id not provided';
+        }
+
+        $cache = get_transient(md5('esoskills_' . serialize($atts)));
+
+        if ($cache) {
+            return $cache;
+        }
+
+        $ch = curl_init('https://eso-skillbook.com/api/tooltip/' . $atts['id']);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $ex = curl_exec($ch);
+        $result = json_decode($ex, true);
+        curl_close($ch);
+
+        if (!isset($result['name']) || !isset($result['tooltip'])) {
+            if (isset($atts['setname'])) {
+                return $atts['setname'];
+            } else {
+                return 'An error occurred retrieving the skill data';
+            }
+        }
+
+        $tooltip = str_replace('"', "'", $result['tooltip']);
+        if (isset($atts['skillname'])) {
+            $name = $atts['skillname'];
+        } else {
+            $name = $result['name'];
+        }
+        $return = '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['id'] . '" target="_blank" rel="noopener" ';
+        if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
+            $return .= 'data-toggle="tooltip" ';
+        }
+        $return .= 'data-html="true" title="' . $tooltip . '">' . $name . '</a>';
+
+        if (!is_preview()) {
+
+            set_transient(md5('esoskills_' . serialize($atts)), $return, 3600);
 
         }
         return $return;
