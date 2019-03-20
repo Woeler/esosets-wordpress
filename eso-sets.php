5eca67025bb3dd13da902f07bdbe6747ccbe4187
Woeler <woeler@esoleaderboards.com>
2019-03-20 11:16:34 +0100

    Add htmlspecial chars and change urls to beast

--- eso-sets.php
+++ eso-sets.php
@@ -53,12 +53,7 @@ final class EsoSets
             return $cache;
         }
 
-        $ch = curl_init('https://eso-sets.com/api/tooltip/set/' . $atts['id']);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $ex = curl_exec($ch);
-        $result = json_decode($ex, true);
-        curl_close($ch);
+        $result = $this->runCurl('https://beast.pathfindermediagroup.com/api/eso/sets/tooltip/' . $atts['id']);
 
         if (!isset($result['name']) || !isset($result['tooltip'])) {
             if (isset($atts['setname'])) {
@@ -78,13 +73,12 @@ final class EsoSets
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip . '">' . $name . '</a>';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip) . '">' . $name . '</a>';
 
         if (!is_preview()) {
-
             set_transient(md5('esosets_' . serialize($atts)), $return, 3600);
-
         }
+
         return $return;
     }
 
@@ -100,12 +94,7 @@ final class EsoSets
             return $cache;
         }
 
-        $ch = curl_init('https://eso-skillbook.com/api/tooltip/' . $atts['id']);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $ex = curl_exec($ch);
-        $result = json_decode($ex, true);
-        curl_close($ch);
+        $result = $this->runCurl('https://beast.pathfindermediagroup.com/api/eso/skills/tooltip/' . $atts['id']);
 
         if (!isset($result['name']) || !isset($result['tooltip'])) {
             if (isset($atts['setname'])) {
@@ -125,12 +114,10 @@ final class EsoSets
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip . '">' . $name . '</a>';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip) . '">' . $name . '</a>';
 
         if (!is_preview()) {
-
             set_transient(md5('esoskills_' . serialize($atts)), $return, 3600);
-
         }
 
         return $return;
@@ -152,13 +139,7 @@ final class EsoSets
         $data['skill_5'] = $atts['skill_5'];
         $data['skill_ult'] = $atts['skill_ult'];
 
-        $ch = curl_init('https://eso-skillbook.com/api/skillbar');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $ex = curl_exec($ch);
-        $result = json_decode($ex, true);
-        curl_close($ch);
+        $result = $this->runCurl('https://beast.pathfindermediagroup.com/api/eso/skills/skillbar');
 
         $tooltip_1 = str_replace('"', "'", $result['skill_1']['tooltip']);
         $tooltip_2 = str_replace('"', "'", $result['skill_2']['tooltip']);
@@ -172,40 +153,56 @@ final class EsoSets
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_1 . '"><img class="skill-img" width="50px" src="' . $result['skill_1']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_1) . '"><img class="skill-img" width="50px" src="' . $result['skill_1']['img'] . '" /></a> ';
         $return .= '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['skill_2'] . '" target="_blank" ';
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_2 . '"><img class="skill-img" width="50px" src="' . $result['skill_2']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_2) . '"><img class="skill-img" width="50px" src="' . $result['skill_2']['img'] . '" /></a> ';
         $return .= '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['skill_3'] . '" target="_blank" ';
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_3 . '"><img class="skill-img" width="50px" src="' . $result['skill_3']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_3) . '"><img class="skill-img" width="50px" src="' . $result['skill_3']['img'] . '" /></a> ';
         $return .= '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['skill_4'] . '" target="_blank" ';
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_4 . '"><img class="skill-img" width="50px" src="' . $result['skill_4']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_4) . '"><img class="skill-img" width="50px" src="' . $result['skill_4']['img'] . '" /></a> ';
         $return .= '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['skill_5'] . '" target="_blank" ';
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_5 . '"><img class="skill-img" width="50px" src="' . $result['skill_5']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_5) . '"><img class="skill-img" width="50px" src="' . $result['skill_5']['img'] . '" /></a> ';
         $return .= '<a class="eso-set" href="https://www.eso-skillbook.com/skill/' . $atts['skill_ult'] . '" target="_blank" ';
         if (isset($atts['tooltip']) && $atts['tooltip'] == 'true') {
             $return .= 'data-toggle="tooltip" ';
         }
-        $return .= 'data-html="true" title="' . $tooltip_ult . '"><img class="skill-img" style="margin-left:25px;" width="50px" src="' . $result['skill_ult']['img'] . '" /></a> ';
+        $return .= 'data-html="true" title="' . htmlspecialchars($tooltip_ult) . '"><img class="skill-img" style="margin-left:25px;" width="50px" src="' . $result['skill_ult']['img'] . '" /></a> ';
         $return .= '</div>';
 
         if (!is_preview()) {
-
             set_transient(md5('esoskillsbar_' . serialize($atts)), $return, 3600);
-
         }
+
         return $return;
+    }
+
+    protected function runCurl($url, $data = [])
+    {
+        if (!empty($data)) {
+            $query = '?' . http_build_query($data);
+        } else {
+            $query = '';
+        }
+        $ch = curl_init($url . $query);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $ex = curl_exec($ch);
+        $result = json_decode($ex, true);
+        curl_close($ch);
+
+        return $result;
     }
 
 
